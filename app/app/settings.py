5ec7d9df50cd2d099e6397321e4a97bd78c39951
poco <poco@email.com>
2025-02-27 11:47:44 +0200

    linked db to settings.py and added core app

--- app/app/settings.py
+++ app/app/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "core"
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,11 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': os.environ.get('DB_HOST'), #This variable doesn't exist in .env file because is defined in docker-compose.yml
+        'NAME': os.environ.get('DB_NAME'), # (same as above)
+        'USER': os.environ.get('DB_USER'), # (same as above)
+        'PASSWORD': os.environ.get('DB_PASS'), # (same as above)
     }
 }
 
